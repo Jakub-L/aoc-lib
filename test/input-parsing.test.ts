a6fb89efd6ddbd9f8a6cad7a8634b520e6c330ff
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-11-07 22:33:43 +0000

    Update test to jest format

--- test/input-parsing.test.ts
+++ test/input-parsing.test.ts
@@ -1,8 +1,7 @@
-import { expect } from "chai";
 import { split } from "../src/input-parsing/read-file";
 
 describe("split", () => {
   it("returns the original string if given no delimiters", () => {
-    expect(split("Hello World")).to.equal("Hello World");
+    expect(split("Hello World")).toBe("Hello World");
   });
 });
