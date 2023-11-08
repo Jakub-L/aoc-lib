90fa6b1c2b503fe4ef41e9590bf10c5824002cf6
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-11-08 21:20:07 +0000

    Add pathfinding submodule

---
+++ src/pathfinding/dijkstra.ts
@@ -0,0 +1,54 @@
+type GridDijkstraOptions = {
+  start?: number[];
+  end?: number[];
+  allowDiagonals?: boolean;
+};
+
+/**
+ * Searches through a grid of numbers using Dijkstra's algorithm to find the shortest path between
+ * the start and end. Assumes only vertical and horizontal movement is allowed.
+ * @param {number[][]} grid - 2D array of numbers representing the cost to enter a grid point
+ * @param {GridDijkstraOptions} options - Options for the search
+ * @param {number[]} [options.start=[0,0]] - The starting coordinates of the search [y, x]
+ * @param {number[]} [options.end=[grid.length - 1, grid[0].length - 1]] - The end coordinates of the search [y, x]
+ * @param {boolean} [options.allowDiagonals=false] - Whether or not to allow diagonal movement
+ * @returns {number} The distance from start to end, or -1 if no path could be found
+ */
+export const gridDijkstra = (grid: number[][], options: GridDijkstraOptions = {}): number => {
+  const { start = [0, 0], end = [grid.length - 1, grid[0].length - 1], allowDiagonals = false } = options;
+  const neighbours = getNeighbours(allowDiagonals);
+  const visited = new Set();
+  const [y0, x0] = start;
+  const [yt, xt] = end;
+  const queue = [{ x: x0, y: y0, dist: 0 }];
+
+  while (queue.length) {
+    const { x, y, dist } = queue.shift();
+    // If we made it to the end, return distance to it
+    if (x === xt && y === yt) return dist;
+    // Otherwise, check every neighbour
+    for (let [dx, dy] of neighbours) {
+      const [xx, yy] = [x + dx, y + dy];
+      // If point is outside grid bounds or has been visited, skip it
+      if (grid[yy]?.[xx] === undefined || visited.has(`${xx},${yy}`)) continue;
+      // Otherwise mark the point as visited and add it to the queue of vertices to check
+      visited.add(`${xx},${yy}`);
+      queue.push({ x: xx, y: yy, dist: dist + grid[yy][xx] });
+    }
+    // Sort the queue to find the shortest-distance vertex to inspect next
+    queue.sort((a, b) => a.dist - b.dist);
+  }
+  return -1;
+};
+
+/**
+ * Returns an array of neighbouring coordinates for a given cell, optionally including diagonals.
+ * @param allowDiagonals - Whether or not to include diagonal neighbours.
+ * @returns An array of neighbouring coordinates.
+ */
+export const getNeighbours = (allowDiagonals: boolean) => {
+  // prettier-ignore
+  return allowDiagonals
+    ? [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+    : [[-1, 0], [0, -1], [0, 1], [1, 0]];
+};
---
+++ src/pathfinding/index.ts
@@ -0,0 +1,1 @@
+export { gridDijkstra } from "./dijkstra";
