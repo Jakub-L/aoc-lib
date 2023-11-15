0aef87cfd3e9a862858fbef7a5db7426065ead4b
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-11-15 21:28:19 +0000

    Add greatest common divisor function

--- src/math/index.ts
+++ src/math/index.ts
@@ -1,15 +1,26 @@
 /**
  * Returns the sum of all numbers in the given array.
  *
- * @param arr - The array of numbers to sum.
- * @returns The sum of all numbers in the array.
+ * @param {number[]} arr The array of numbers to sum.
+ * @returns {number} The sum of all numbers in the array.
  */
 export const sum = (arr: number[]): number => arr.reduce((a, b) => a + b, 0);
 
 /**
  * Returns the product of all numbers in the given array.
  *
- * @param arr - The array of numbers to multiply.
- * @returns The product of all numbers in the array.
+ * @param {number[]} arr The array of numbers to multiply.
+ * @returns {number} The product of all numbers in the array.
  */
 export const prod = (arr: number[]): number => arr.reduce((a, b) => a * b, 1);
+
+/**
+ * Calculates the greatest common divisor of two numbers.
+ * @param {number} a First number
+ * @param {number} b Second number
+ * @returns {number} The greatest common divisor of a and b.
+ */
+export const gcd = (a: number, b: number): number => {
+  if (b === 0) return Math.abs(a);
+  return gcd(Math.abs(b), Math.abs(a % b));
+}
--- test/math.test.ts
+++ test/math.test.ts
@@ -1,4 +1,4 @@
-import { sum, prod } from "../src/math";
+import { gcd, prod, sum } from "../src/math";
 
 describe("sum", () => {
   it("returns 0 for empty array", () => {
@@ -9,10 +9,10 @@ describe("sum", () => {
   });
   it("sums positive integers", () => {
     expect(sum([1, 2, 3])).toEqual(6);
-  })
+  });
   it("sums positive and negative integers", () => {
     expect(sum([-1, 2, -3])).toEqual(-2);
-  })
+  });
   it("sums floats", () => {
     expect(sum([1.5, 2.5, 3.5])).toEqual(7.5);
     expect(sum([1.5, -2.5, 3.5])).toEqual(2.5);
@@ -28,15 +28,43 @@ describe("prod", () => {
   });
   it("sums positive integers", () => {
     expect(prod([8, 3, 5])).toEqual(120);
-  })
+  });
   it("sums positive and negative integers", () => {
     expect(prod([-3, 2, -5])).toEqual(30);
     expect(prod([3, 2, -5])).toEqual(-30);
     expect(prod([3, 2, 5])).toEqual(30);
-  })
+  });
   it("sums floats", () => {
     expect(prod([1.5, 2.5, 3.5])).toEqual(13.125);
     expect(prod([1.5, -2.5, 3.5])).toEqual(-13.125);
   });
 });
 
+describe("gcd", () => {
+  it("returns 1 for coprime numbers", () => {
+    expect(gcd(5, 7)).toEqual(1);
+  });
+  it("returns 1 for 1 and any number", () => {
+    expect(gcd(1, 7)).toEqual(1);
+  });
+  it("returns largest number if one is multiple of the other", () => {
+    expect(gcd(5, 10)).toEqual(5);
+  });
+  it("returns gcd for two numbers", () => {
+    expect(gcd(12, 18)).toEqual(6);
+  });
+  it("returns the number if both are equal", () => {
+    expect(gcd(5, 5)).toEqual(5);
+  });
+  it("returns positive gcd for negative numbers", () => {
+    expect(gcd(-12, 18)).toEqual(6);
+    expect(gcd(12, -18)).toEqual(6);
+    expect(gcd(-12, -18)).toEqual(6);
+  });
+  it("returns the same gcd regardless of order", () => {
+    expect(gcd(12, 18)).toEqual(gcd(18, 12));
+  });
+  it("returns gcd for floats", () => {
+    expect(gcd(1.5, 4.5)).toEqual(1.5);
+  })
+});
