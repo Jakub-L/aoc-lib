893ff7d7a225f1c91f8992206cb5aef8a59a8033
Jakub L. <47220449+Jakub-L@users.noreply.github.com>
2024-12-01 21:38:47 +0000

    Allow read-file's Element to have any type to make result casting easier

--- src/input-parsing/read-file.ts
+++ src/input-parsing/read-file.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from "fs";
 
-type Element = Array<Element> | string;
 type Converter = (x: string) => any;
 const identity = <T>(x: T): T => x;
 
@@ -9,9 +8,9 @@ const identity = <T>(x: T): T => x;
  * @param {string} path - The path of the file to read.
  * @param {string[]} [delimiters=["\n"]] - An array of delimiters to split the file contents by. Defaults to ["\n"].
  * @param {Converter} [converter=identity] - A function to apply to the file contents after splitting. Defaults to the identity function.
- * @returns {Element[]} An array of elements representing the contents of the file split by the given delimiters.
+ * @returns {any} An array of elements representing the contents of the file split by the given delimiters.
  */
-export const readFile = (path: string, delimiters: string[] = ["\n"], converter: Converter = identity): Element => {
+export const readFile = (path: string, delimiters: string[] = ["\n"], converter: Converter = identity): any => {
   return split(readFileSync(path, "utf-8"), delimiters, converter);
 };
 
@@ -20,9 +19,9 @@ export const readFile = (path: string, delimiters: string[] = ["\n"], converter:
  * @param {string} str - The string to split.
  * @param {string[]} [delimiters=[]] - An array of delimiters to split the string by.
  * @param {Converter} [converter=identity] - A function to apply to the file contents after splitting. Defaults to the identity function.
- * @returns {Element[]} An array of elements representing the string split by the given delimiters.
+ * @returns {any} An array of elements representing the string split by the given delimiters.
  */
-export const split = (str: string, delimiters: string[] = [], converter: Converter = identity): Element => {
+export const split = (str: string, delimiters: string[] = [], converter: Converter = identity): any => {
   if (delimiters.length === 0) return converter(str);
   const [delimiter, ...remainingDelimiters] = delimiters;
   return str.split(delimiter).map(s => split(s, remainingDelimiters, converter));
